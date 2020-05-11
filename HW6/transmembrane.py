aa331c288f192b90cbf332601bbf40dede8f8cee
Lily Karim <lilyk@Lilys-MacBook-Pro.local>
2020-05-10 19:41:10 -0700

    HW 6

---
+++ HW6/transmembrane.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+
+import gzip
+import sys
+
+# Write a program that predicts if a protein is trans-membrane
+# Trans-membrane proteins have the following properties
+#	Signal peptide: https://en.wikipedia.org/wiki/Signal_peptide
+#	Hydrophobic regions(s): https://en.wikipedia.org/wiki/Transmembrane_protein
+#	No prolines (alpha helix)
+# Hydrophobicity is measued via Kyte-Dolittle
+#	https://en.wikipedia.org/wiki/Hydrophilicity_plot
+# For our purposes:
+#	Signal peptide is 8 aa long, KD > 2.5, first 30 aa
+#	Hydrophobic region is 11 aa long, KD > 2.0, after 30 aa
+
+def read_fasta(filename):
+	name = None
+	seqs = []
+	
+	fp = None
+	if filename == '-':
+		fp = sys.stdin
+	elif filename.endswith('.gz'):
+		fp = gzip.open(filename, 'rt')
+	else:
+		fp = open(filename)
+
+	for line in fp.readlines():
+		line = line.rstrip()
+		if line.startswith('>'):
+			if len(seqs) > 0:
+				seq = ''.join(seqs)
+				yield(name, seq)
+				name = line[1:]
+				seqs = []
+			else:
+				name = line[1:]
+		else:
+			seqs.append(line)
+	yield(name, ''.join(seqs))
+	fp.close()
+	
+def KD(peptide):
+	kd = 0
+	for aa in peptide:
+		if aa == 'I': kd += 4.5
+		elif aa == 'V': kd += 4.2
+		elif aa == 'L': kd += 3.8
+		elif aa == 'F': kd += 2.8
+		elif aa == 'C': kd += 2.5
+		elif aa == 'M': kd += 1.9
+		elif aa == 'A': kd += 1.8
+		elif aa == 'G': kd += -.4
+		elif aa == 'T': kd += -.7
+		elif aa == 'S': kd += -.8
+		elif aa == 'W': kd += -.9
+		elif aa == 'Y': kd += -1.3
+		elif aa == 'P': kd += -1.6
+		elif aa == 'E': kd += -3.2
+		elif aa == 'Q': kd += -3.5
+		elif aa == 'D': kd += -3.5
+		elif aa == 'N': kd += -3.5
+		elif aa == 'K': kd += -3.9
+		elif aa == 'R': kd += -4.5
+		else:           return kd/len(peptide)
+	return kd/len(peptide)		
+	
+def window(seq, w, threshold):
+	for i in range(len(seq)-w+1):
+		kmer = seq[i:i+w]
+		score = KD(kmer)
+		if score > threshold and 'P' not in kmer:
+			return True
+	return False
+		
+	
+
+for name, seq in read_fasta('proteins.fasta.gz'):
+	if window(seq[0:30],8,2.5) and window(seq[30:], 11, 2.0):
+		print(name)
+	
+
+"""
+18w
+Dtg
+Krn
+Lac
+Mcr
+PRY
+Pxt
+Pzl
+QC
+Ror
+S1P
+S2P
+Spt
+apn
+bai
+bdl
+bou
+bug
+cue
+drd
+ft
+grk
+knk
+ksh
+m
+nac
+ort
+rk
+smo
+thw
+tsg
+waw
+zye
+"""
